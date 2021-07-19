a31920e9105a38d8867c0b38d0850633d91ed92d
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-07-19 12:58:09 +0100

    Adding fine-tuning model.

---
+++ macular_chatbot/tasks/data_preparation/prepare_data_liveqa_task.py
@@ -0,0 +1,47 @@
+from prefect import Task
+from loguru import logger
+import pandas as pd
+import random
+import xml.etree.ElementTree as ET
+from macular_chatbot.util import gen_negative_pairs
+from tqdm import tqdm
+
+
+class PrepareDataLiveQATask(Task):
+    def run(self, input_file_location):
+        logger.info("** Preparing LiveQA dataset **")
+        tree = ET.parse(input_file_location)
+        root = tree.getroot()
+
+        positive_pairs = list()
+        all_answers = list()
+
+        # read the files
+        for content in root.findall("NLM-QUESTION"):
+            question = content.find("MESSAGE").text
+            if not question:
+                question = content.find("SUBJECT").text
+            answer = (
+                content.find("SUB-QUESTIONS")
+                .find("SUB-QUESTION")
+                .find("ANSWERS")
+                .find("ANSWER")
+                .text
+            )
+            positive_pairs.append([question, answer])
+            all_answers.append(answer)
+
+        # gen the negative pairs
+        negative_pairs = []
+        logger.info("** Generating negative pairs **")
+        for p in tqdm(positive_pairs):
+            negative_pairs.extend(
+                gen_negative_pairs(
+                    question=p[0], all_answers=all_answers, positive_answer=p[1]
+                )
+            )
+
+        logger.info(f"Total negative: {len(negative_pairs)}")
+        logger.info(f"Total positive: {len(positive_pairs)}")
+
+        return {"negative": negative_pairs, "positive": positive_pairs}
--- macular_chatbot/tasks/transformers/SentenceBertKB.py
+++ macular_chatbot/tasks/transformers/SentenceBertKB.py
@@ -3,8 +3,8 @@ from sentence_transformers import SentenceTransformer, util
 
 
 class SentenceBertKB:
-    def __init__(self, input_data):
-        self.model = SentenceTransformer("distilbert-base-nli-stsb-mean-tokens")
+    def __init__(self, input_data, model):
+        self.model = SentenceTransformer(model)
 
         all_questions = [
             content["question"] for id_question, content in input_data.items()
@@ -49,3 +49,52 @@ class SentenceBertKB:
             retrieved_answer = "I don't know the answer to that one! :("
 
         return retrieved_answer, probability_retrieved_answer, extra_info
+
+
+# class SentenceBertKB:
+#     def __init__(self, input_data):
+#         self.model = SentenceTransformer("distilbert-base-nli-stsb-mean-tokens")
+
+#         all_questions = [
+#             content["question"] for id_question, content in input_data.items()
+#         ]
+#         all_answers = [content["answer"] for id_question, content in input_data.items()]
+#         all_extra = [
+#             content["extra_info"] for id_question, content in input_data.items()
+#         ]
+#         self.embeddings = self.model.encode(all_questions)
+
+#         self.kb = dict()
+
+#         for question, answer, embedding, extra_info in zip(
+#             all_questions, all_answers, self.embeddings, all_extra
+#         ):
+#             self.kb[len(self.kb)] = {
+#                 "question": question,
+#                 "answer": answer,
+#                 "embedding": embedding,
+#                 "extra_info": extra_info,
+#             }
+
+#     def encode_sentence(self, input_question):
+#         question_embedding = self.model.encode(input_question)
+
+#         return question_embedding
+
+#     def get_closest(self, input_question_embedding):
+#         hits = util.semantic_search(
+#             input_question_embedding, self.embeddings, score_function=util.cos_sim
+#         )
+
+#         retrieved_answer = self.kb[hits[0][0]["corpus_id"]]["answer"]
+
+#         probability_retrieved_answer = hits[0][0]["score"]
+
+#         extra_info = self.kb[hits[0][0]["corpus_id"]]["extra_info"]
+
+#         print(probability_retrieved_answer)
+
+#         if probability_retrieved_answer < 0.60:
+#             retrieved_answer = "I don't know the answer to that one! :("
+
+#         return retrieved_answer, probability_retrieved_answer, extra_info
