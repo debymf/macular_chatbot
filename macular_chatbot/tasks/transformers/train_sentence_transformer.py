8ce548d681598b75bf4be63196dec683ae7bd854
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-07-19 19:35:57 +0100

    adding argparse for models.

--- macular_chatbot/tasks/transformers/train_sentence_transformer.py
+++ macular_chatbot/tasks/transformers/train_sentence_transformer.py
@@ -19,7 +19,7 @@ class TrainSentenceTransformerTask(Task):
         # Tune the model
         model.fit(
             train_objectives=[(data_for_training["dataloader"], train_loss)],
-            epochs=1,
+            epochs=10,
             warmup_steps=10,
             output_path=output_location,
             save_best_model=True,
