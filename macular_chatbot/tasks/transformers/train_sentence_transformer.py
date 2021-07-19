05b6baf1bf50cad06ab2480e583c6afc01b27992
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-07-19 15:28:25 +0100

    adding argparse for models.

--- macular_chatbot/tasks/transformers/train_sentence_transformer.py
+++ macular_chatbot/tasks/transformers/train_sentence_transformer.py
@@ -1,21 +1,28 @@
 from prefect import Task
 from loguru import logger
-from sentence_transformers import SentenceTransformer, util, losses
+from sentence_transformers import SentenceTransformer, util, losses, evaluation
 from .SentenceBertKB import SentenceBertKB
 from tqdm import tqdm
 
 
 class TrainSentenceTransformerTask(Task):
-    def run(self, dataloader, model_name, output_location):
+    def run(self, data_for_training, model_name, output_location):
+        evaluator = evaluation.EmbeddingSimilarityEvaluator(
+            data_for_training["sentences1"],
+            data_for_training["sentences2"],
+            data_for_training["scores"],
+        )
 
         model = SentenceTransformer(model_name)
         train_loss = losses.CosineSimilarityLoss(model)
 
         # Tune the model
         model.fit(
-            train_objectives=[(dataloader, train_loss)],
+            train_objectives=[(data_for_training["dataloader"], train_loss)],
             epochs=5,
             warmup_steps=100,
             output_path=output_location,
             save_best_model=True,
+            evaluator=evaluator,
+            evaluation_steps=500,
         )
