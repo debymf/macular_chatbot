79000927ccebc17be4197285310d79097c7cc69f
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-07-19 15:58:40 +0100

    adding argparse for models.

--- macular_chatbot/util/negative_sampling.py
+++ macular_chatbot/util/negative_sampling.py
@@ -1,15 +1,28 @@
 from loguru import logger
 import random
+from .bm25 import BM25Fit, BM25Search
 
 
 def gen_negative_pairs(question, all_answers, positive_answer, num_neg=50):
-    no_duplicate = 1
+
+    fit_class = BM25Fit()
+    ix = fit_class.run(all_answers)
+    search_class = BM25Search()
+    retrieval_results = search_class.run({"id": positive_answer}, ix, limit=num_neg)
+    closest = list(retrieval_results["id"].keys())[1:]
+
+    # negative_pairs = []
+    # while no_duplicate == 1:
+    #     negative_selected = random.sample(all_answers, num_neg)
+    #     if positive_answer not in negative_selected:
+    #         no_duplicate = 0
+    #         for n in negative_selected:
+    #             negative_pairs.append([question, n])
+
+    # return negative_pairs
+
     negative_pairs = []
-    while no_duplicate == 1:
-        negative_selected = random.sample(all_answers, num_neg)
-        if positive_answer not in negative_selected:
-            no_duplicate = 0
-            for n in negative_selected:
-                negative_pairs.append([question, n])
+    for n in closest:
+        negative_pairs.append([question, all_answers[n]])
 
     return negative_pairs
