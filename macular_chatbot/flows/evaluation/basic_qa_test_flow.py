31061fa1b489583b79b71be01da133c18b740b21
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-07-20 11:19:44 +0100

    adding argparse for models.

--- macular_chatbot/flows/evaluation/basic_qa_test_flow.py
+++ macular_chatbot/flows/evaluation/basic_qa_test_flow.py
@@ -18,50 +18,10 @@ file_location = settings["basic_qa_data"]
 # USED_MODEL = "paraphrase-mpnet-base-v2"
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--model",
-    metavar="Model name",
-    type=str,
-    nargs="?",
-    help="sentence embedding model",
-    default="msmarco-distilbert-base-v4",
-)
-
-parser.add_argument(
-    "--scoring",
-    metavar="Scoring function",
-    type=str,
-    nargs="?",
-    help="Scoring function",
-    default="cos",
-)
-
-
-args = parser.parse_args()
-
-scoring_function = args.scoring
-
-if "cos" in scoring_function:
-    score_function = util.cos_sim
-else:
-    score_function = util.dot_score
-
-
-USED_MODEL = args.model
-
-cache_args = dict(
-    target="{task_name}-{task_tags}.pkl",
-    checkpoint=True,
-    result=LocalResult(dir=f"{checkpoint_dir}/{TASK_NAME}"),
-)
-
-prepare_data_task = PrepareDataTask()
-convert_to_vectors = ConvertToVectorsTask()
-evaluate_vectors = EvaluateVectorsTask()
-
-
 def run_test_flow(used_model, score_function):
+    prepare_data_task = PrepareDataTask()
+    convert_to_vectors = ConvertToVectorsTask()
+    evaluate_vectors = EvaluateVectorsTask()
     with Flow("Testing model without fine-tuning") as flow1:
         input_data = prepare_data_task(file_location)
         output_vectors = convert_to_vectors(input_data, USED_MODEL)
@@ -71,4 +31,39 @@ def run_test_flow(used_model, score_function):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model",
+        metavar="Model name",
+        type=str,
+        nargs="?",
+        help="sentence embedding model",
+        default="msmarco-distilbert-base-v4",
+    )
+
+    parser.add_argument(
+        "--scoring",
+        metavar="Scoring function",
+        type=str,
+        nargs="?",
+        help="Scoring function",
+        default="cos",
+    )
+
+    args = parser.parse_args()
+
+    scoring_function = args.scoring
+
+    if "cos" in scoring_function:
+        score_function = util.cos_sim
+    else:
+        score_function = util.dot_score
+
+    USED_MODEL = args.model
+
+    cache_args = dict(
+        target="{task_name}-{task_tags}.pkl",
+        checkpoint=True,
+        result=LocalResult(dir=f"{checkpoint_dir}/{TASK_NAME}"),
+    )
     run_test_flow(used_model=USED_MODEL, score_function=score_function)
