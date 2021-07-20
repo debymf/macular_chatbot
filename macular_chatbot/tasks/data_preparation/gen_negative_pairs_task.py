141a32dfb197bee9e64d178c90e4c22d9d8dce87
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-07-20 09:06:33 +0100

    adding argparse for models.

---
+++ macular_chatbot/tasks/data_preparation/gen_negative_pairs_task.py
@@ -0,0 +1,39 @@
+from prefect import Task
+from loguru import logger
+import pandas as pd
+import random
+import xml.etree.ElementTree as ET
+from macular_chatbot.util import gen_negative_pairs
+from tqdm import tqdm
+import glob, os
+
+
+class GenNegativePairsTask(Task):
+    def run(self, positive_pairs):
+        logger.info("** Generating negative pairs **")
+
+        # gen the negative pairs
+        all_answers = dict()
+        all_questions = dict()
+        count = 0
+        logger.info("** Generating negative pairs **")
+        for p in tqdm(positive_pairs):
+            all_questions[count] = p[0]
+            all_answers[count] = p[1]
+            count = count + 1
+
+        retrieved_results = gen_negative_pairs(
+            questions=all_questions, all_answers=all_answers
+        )
+
+        count = 0
+        negative_pairs = []
+        for i in range(0, len(all_questions)):
+            closest = list(retrieval_results[i].keys())[1:]
+            for n in closest:
+                negative_pairs.append([all_questions[i], all_answers[n]])
+
+        logger.info(f"Total negative: {len(negative_pairs)}")
+        logger.info(f"Total positive: {len(positive_pairs)}")
+
+        return {"negative": negative_pairs, "positive": positive_pairs}
