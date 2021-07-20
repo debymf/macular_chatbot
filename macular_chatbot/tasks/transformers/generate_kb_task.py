9280d58377114b4227b3b507df662f08470fc362
Deborah Ferreira <deborahmdf@gmail.com>
2021-07-20 20:33:03 +0100

    adding speech chatbot.

--- macular_chatbot/tasks/transformers/generate_kb_task.py
+++ macular_chatbot/tasks/transformers/generate_kb_task.py
@@ -5,8 +5,8 @@ from .SentenceBertKB import SentenceBertKB
 
 
 class GenerateKBTask(Task):
-    def run(self, input_data):
+    def run(self, input_data, model_name):
         logger.info("** Converting Sentences to Vector **")
-        kb = SentenceBertKB(input_data)
+        kb = SentenceBertKB(input_data, model_name)
 
         return kb
