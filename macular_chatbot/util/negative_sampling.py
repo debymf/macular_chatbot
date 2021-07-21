36d11c7801e4a04169798320be32cf2838d3deae
Deborah Ferreira <deborah.ferreira@manchester.ac.uk>
2021-07-21 09:07:42 +0100

    adding argparse for models.

--- macular_chatbot/util/negative_sampling.py
+++ macular_chatbot/util/negative_sampling.py
@@ -3,7 +3,7 @@ import random
 from .bm25 import BM25Fit, BM25Search
 
 
-def gen_negative_pairs(questions, all_answers, num_neg=10):
+def gen_negative_pairs(questions, all_answers, num_neg=50):
 
     fit_class = BM25Fit()
     ix = fit_class.run(all_answers)
